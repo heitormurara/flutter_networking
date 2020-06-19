27ae8b926f15d18689fa0cfd57f56bd9640c9911
Heitor Murara <heitormurara@gmail.com>
2020-06-19 03:18:50 -0300

    Create Data Model Classes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/heitormurara/Projects/utils/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/heitormurara/Projects/Flutter/flutter_networking"
-export "FLUTTER_TARGET=/Users/heitormurara/Projects/Flutter/flutter_networking/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/heitormurara/Projects/Flutter/flutter_networking+api/flutter_networking"
+export "FLUTTER_TARGET=/Users/heitormurara/Projects/Flutter/flutter_networking+api/flutter_networking/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
